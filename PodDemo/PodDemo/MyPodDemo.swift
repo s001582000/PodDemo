a4fde8ee982b6fa888d939c23169b6bf4a553815
梁雅軒 <s001582000@gmail.com>
2019-02-27 09:02:52 +0800

    init

---
+++ PodDemo/PodDemo/MyPodDemo.swift
@@ -0,0 +1,15 @@
+//
+//  MyPodDemo.swift
+//  PodDemo
+//
+//  Created by YASYUAN LINANG on 2019/2/27.
+//  Copyright © 2019 YASYUAN LINANG. All rights reserved.
+//
+
+import UIKit
+
+class MyPodDemo {
+    func returnStr() -> String {
+        return "你好"
+    }
+}
---
+++ PodDemo/PodDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodDemo
+//
+//  Created by YASYUAN LINANG on 2019/2/27.
+//  Copyright © 2019 YASYUAN LINANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
